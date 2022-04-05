c19ec534de4dd36159b46d2383ed0609ef95a3c0
Heidi Sánchez <heidisanchez103.9@gmail.com>
2022-04-04 22:27:18 -0500

    He mejorado las funciones de mi lista de contactos a partir de un objeto

--- scripts.js
+++ scripts.js
@@ -6,31 +6,71 @@ Publica tu código en Github, en tu repositorio contact-list crea un branch llam
 
 
 
-/*Mi lista predefinida*/
-let myLista = ["Heidi Sánchez","Jesús Naja", "Hector Sánchez"];
+
 
 /*Función de incluir contacto a mi lista*/
 
-function incluirContacto (){
-  myLista.push ("Aithana Chaves");  
+function addContact (){
+  const contact = 
+    {
+    id: parseInt(prompt("Ingrese un número de id")),
+    name: prompt("Ingrese el nombre del contacto"),
+    surname: prompt("Ingrese el apellido del contacto"),
+    cellphone: parseInt(prompt("Ingrese un número de teléfono")),
+    location: [prompt("Ingrese Ciudad"), prompt("Ingrese país")]
+    }
+  
+
+  myList.push (contact);  
 }
+
+
 
 /*Crea una función para borrar un contacto existente de la lista*/
 
-function borrarContacto (){
-  myLista.pop ("Aithana Chaves"); 
+function eraseContact (){
+  const position = parseInt(prompt(" Introducir el número de la posición del contacto que desea borrar"));
+  myList.splice(position, 1); 
 }
 /*Crea una función para imprimir en consola los contactos presentes en la lista*/
-function imprimir() {
-    console.log (myLista);
+function print() {
+    console.table (myList);
   }
 
 /* Se inicia la ejecución de las funciones*/
+/*Mi lista predefinida*/
 
-incluirContacto();
-imprimir();
+let  myList = [
+  {
+   id: 1,
+   name: "Heidi",
+   surname: "Sanchez",
+   cellphone: 099864455,
+   location: ["Quito", "Azcasubi"]
+  },
+  {
+    id: 2,
+    name: "Moises",
+    surname: "Dupuy",
+    cellphone: 099869955,
+    location: ["Caracas", "Venezuela"]
+   },
+   {
+    id: 3,
+    name: "Nelly",
+    surname: "García",
+    cellphone: 099869111,
+    location: ["Guayaquil", "Ecuador"]
+   }
+  ];
 
-borrarContacto();
-imprimir();
+print();
+addContact();
+print();
+eraseContact();
+print();
+
+
+
 
 
